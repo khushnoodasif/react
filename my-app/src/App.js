b8f5025ddda7bef5698fa549b823f915a7dfcf42
Khushnood Asif <khushnood99@gmail.com>
2021-11-23 23:37:46 +0000

    added dynamic h2 heading

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,17 @@ const fName = "Khushnood";
 const lName = "Asif";
 const currentDate = new Date();
 const year = currentDate.getFullYear();
+const currentTime = currentDate.getHours();
+
+let greeting;
+
+if (currentTime < 12) {
+  greeting = "Good Morning!";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon!";
+} else {
+  greeting = "Good Evening!";
+}
 
 function App() {
   return (
@@ -13,6 +24,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <div>
           <h1>Hello {fName + " " + lName}!</h1>
+          <h2>{greeting}</h2>
           <p>Your lucky number is {Math.floor(Math.random()*60)}</p>
         </div>
       </header>
