bccfa148dddc0dd72604230e128386bc7078cd0d
Khushnood Asif <khushnood99@gmail.com>
2021-11-23 19:26:32 +0000

    lucky number edit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const fName = "Khushnood";
+const lName = "Asif";
+const num = 30;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <h1>This is my first react app</h1>
-          <ul>
-            <li>Chicken</li>
-            <li>Noodles</li>
-            <li>Pizza</li>
-          </ul>
+          <h1>Hello {fName + " " + lName}!</h1>
+          <p>Your lucky number is {num}</p>
         </div>
       </header>
     </div>
