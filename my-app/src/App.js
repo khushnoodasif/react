94384ddcd0cc4dd14668cc2023831788921579f8
Khushnood Asif <khushnood99@gmail.com>
2021-11-23 23:49:10 +0000

    logo pdated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,7 +23,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <h1>Hello {fName + " " + lName}!</h1>
+          <h1>Hello from {fName + " " + lName}!</h1>
           <h2>{greeting}</h2>
           <p>Your lucky number is {Math.floor(Math.random()*60)}</p>
         </div>
