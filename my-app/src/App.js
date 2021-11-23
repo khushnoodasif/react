a88721ec3ffd2f87a58cfc4cc624691f3bb26721
Khushnood Asif <khushnood99@gmail.com>
2021-11-23 19:10:33 +0000

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <h1>Hello World!</h1>
+          <p>This is a paragraph.</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
